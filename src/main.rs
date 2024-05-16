ae4f554f943c48fc855ff480ab64061f53d6b7fa
Pietro Agnoli <thecaptaincraken@gmail.com>
2024-05-16 12:47:21 +0200

    added rodio

--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,32 @@
-use std::io;
+use std::io::BufReader;
+use std::{env, fs, io::Error, path::PathBuf};
 
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
-use ratatui::{
-    prelude::*,
-    symbols::border,
-    widgets::{block::*, *},
-};
+fn main() -> Result<(), Error> {
+    let args: Vec<String> = env::args().collect();
 
-mod tui;
+    let folder_path = args.get(1).unwrap();
+    let folder = fs::read_dir(folder_path).unwrap();
 
-fn main() -> io::Result<()> {
-    let mut terminal = tui::init()?;
-    let app_result = App::default().run(&mut terminal);
-    tui::restore()?;
-    app_result
-}
+    let mut files: Vec<PathBuf> = Vec::new();
 
-#[derive(Debug, Default)]
-pub struct App {
-    counter: u8,
-    exit: bool,
-}
-
-impl App {
-    /// runs the application's main loop until the user quits
-    pub fn run(&mut self, terminal: &mut tui::Tui) -> io::Result<()> {
-        while !self.exit {
-            terminal.draw(|frame| self.render_frame(frame))?;
-            self.handle_events()?;
-        }
-        Ok(())
+    for file in folder {
+        files.push(file?.path());
     }
 
-    fn render_frame(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.size());
-    }
+    println!("{:?}", files);
 
-    /// updates the application's state based on user input
-    fn handle_events(&mut self) -> io::Result<()> {
-        match event::read()? {
-            // it's important to check that the event is a key press event as
-            // crossterm also emits key release and repeat events on Windows.
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event)
-            }
-            _ => {}
-        };
-        Ok(())
-    }
+    let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+    let sink = rodio::Sink::try_new(&handle).unwrap();
 
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
-        match key_event.code {
-            KeyCode::Char('q') => self.exit(),
-            KeyCode::Left => self.decrement_counter(),
-            KeyCode::Right => self.increment_counter(),
-            _ => {}
-        }
-    }
+    let file1 = std::fs::File::open(files[1].clone()).unwrap();
+    let file2 = std::fs::File::open(files[1].clone()).unwrap();
+    sink.append(rodio::Decoder::new(BufReader::new(file1)).unwrap());
+    sink.append(rodio::Decoder::new(BufReader::new(file2)).unwrap());
+    std::thread::sleep(std::time::Duration::from_secs(5));
+    sink.pause();
+    std::thread::sleep(std::time::Duration::from_secs(5));
+    sink.play();
+    sink.sleep_until_end();
 
-    fn exit(&mut self) {
-        self.exit = true;
-    }
-
-    fn increment_counter(&mut self) {
-        self.counter += 1;
-    }
-
-    fn decrement_counter(&mut self) {
-        self.counter -= 1;
-    }
-}
-
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Title::from(" Jembo Jam Session ".bold());
-        let instructions = Title::from(Line::from(vec![
-            " Decrement ".into(),
-            "<Left>".blue().bold(),
-            " Increment ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]));
-        let block = Block::default()
-            .title(title.alignment(Alignment::Center))
-            .title(
-                instructions
-                    .alignment(Alignment::Center)
-                    .position(Position::Bottom),
-            )
-            .borders(Borders::ALL)
-            .border_set(border::THICK);
-
-        let counter_text = Text::from(vec![Line::from(vec![
-            "Value: ".into(),
-            self.counter.to_string().yellow(),
-        ])]);
-
-        Paragraph::new(counter_text)
-            .centered()
-            .block(block)
-            .render(area, buf);
-    }
+    Ok(())
 }
